07310e596c2797cb9132904d11b95ecaa28a63d3
Shane Burkhart <shaneburkhart@gmail.com>
2025-05-01 01:00:30 -0500

    add biome

--- packages/app/apps/app/src/App.tsx
+++ packages/app/apps/app/src/App.tsx
@@ -1,13 +1,13 @@
-import React from 'react';
-import { sharedFunction } from '@repo/shared';
+import * as React from "react";
+import { sharedFunction } from "@repo/shared";
 
 const App: React.FC = () => {
-  return (
-    <div>
-      <h1>RSBuild App</h1>
-      <p>{sharedFunction()}</p>
-    </div>
-  );
+	return (
+		<div>
+			<h1>RSBuild App</h1>
+			<p>{sharedFunction()}</p>
+		</div>
+	);
 };
 
-export default App; 
+export default App;
